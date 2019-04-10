acd18d3bf7d887e557f5782451177ef09929326b
Roman Sarviro <roman.sarviro@mail.ru>
2019-04-10 23:55:28 +0300

    The Rectangle component has been added

---
+++ figures/src/components/Figures/RectangleComponent.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="container row">
+    <div class="col-md-12 mb-12">
+
+      <TitleMessage typeFigures='Rectangle'></TitleMessage>
+
+      <div class="row form-content">
+
+        <Form class='col-md-6 mb-6'>
+          inputs
+        </Form>
+        
+        <div class="col-md-6 mb-6 text-center">
+          <FiguresInformation formula='S=a·b' imageName='rectangle.png'></FiguresInformation>
+        </div>
+        
+      </div>
+      
+    </div>
+  </div>
+</template>
+
+<script>
+import TitleMessage from '@/components/Figures/TitleMessage'
+import FiguresInformation from '@/components/Figures/FiguresInformation'
+
+export default {
+  name: 'Rectangle',
+  components: {
+    TitleMessage,
+    FiguresInformation,
+  }
+}
+</script>
+
+<style scope>
+  .form-content{
+    margin: 25px;
+  }
+</style>
