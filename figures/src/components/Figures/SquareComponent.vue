30c76b9cc0b0a1b2cb55a0404e6693f31f026c1b
Roman Sarviro <roman.sarviro@mail.ru>
2019-04-10 23:55:46 +0300

    The Square component has been added

---
+++ figures/src/components/Figures/SquareComponent.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="container row">
+    <div class="col-md-12 mb-12">
+
+      <TitleMessage typeFigures='Square'></TitleMessage>
+
+      <div class="row form-content">
+
+        <Form class='col-md-6 mb-6'>
+          inputs
+        </Form>
+        
+        <div class="col-md-6 mb-6 text-center">
+          <FiguresInformation formula='S=a²' imageName='square.jpg'></FiguresInformation>
+        </div>
+        
+      </div>
+      
+    </div>
+  </div>
+</template>
+
+<script>
+import TitleMessage from '@/components/Figures/TitleMessage'
+import FiguresInformation from '@/components/Figures/FiguresInformation'
+
+export default {
+  name: 'Square',
+  components: {
+    TitleMessage,
+    FiguresInformation,
+  }
+}
+</script>
+
+<style>
+
+</style>
