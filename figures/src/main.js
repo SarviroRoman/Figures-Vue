637567a0d3d7608fa8d83ccf437f5e29eda77e7a
Roman Sarviro <roman.sarviro@mail.ru>
2019-04-10 23:47:47 +0300

    Added nested routes to the AddFigures component

--- figures/src/main.js
+++ figures/src/main.js
@@ -17,16 +17,52 @@ import add from '@/pages/Add'
 import statistics from '@/pages/Statistics'
 import NotFound from '@/components/NotFound'
 
+import CircleComponent from '@/components/Figures/CircleComponent'
+import RectangleComponent from '@/components/Figures/RectangleComponent'
+import SquareComponent from '@/components/Figures/SquareComponent'
+import TriangleComponent from '@/components/Figures/TriangleComponent'
+
 const routes = [
-  { path: '/', name: 'List figures', component: list, meta: {title: 'List figures'} },
-  { path: '/AddFigures', name: 'Add figures', component: add, meta: {title: 'Add figures'} },
-  { path: '/Statistics', name: 'Statistics figures', component: statistics, meta: {title: 'Statistics figures'} },
-  { path: '*', name: '404 Not Found', component: NotFound, meta: {title: '404 Not Found'} },
+  { path: '/', component: list, meta: {title: 'List figures'} },
+  { 
+    path: '/AddFigures',
+    component: add,
+    meta: {title: 'Add figures'},
+    children: [
+      {
+        path: '',
+        component: CircleComponent,
+        meta: {title: 'Add figures | Circle'}
+      },
+      {
+        path: 'Circle',
+        component: CircleComponent,
+        meta: {title: 'Add figures | Circle'}
+      },
+      {
+        path: 'Rectangle',
+        component: RectangleComponent,
+        meta: {title: 'Add figures | Rectangle'}
+      },
+      {
+        path: 'Square',
+        component: SquareComponent,
+        meta: {title: 'Add figures | Square'}
+      },
+      {
+        path: 'Triangle',
+        component: TriangleComponent,
+        meta: {title: 'Add figures | Triangle'}
+      },
+    ]
+  },
+  { path: '/Statistics', component: statistics, meta: {title: 'Statistics figures'} },
+  { path: '*', component: NotFound, meta: {title: '404 Not Found'} },
 ]
 
 const router = new VueRouter({
   mode: 'history',
-  routes,
+  routes: routes,
   linkActiveClass: "active",
   linkExactActiveClass: "exact-active",
 })
--- figures/src/pages/Add.vue
+++ figures/src/pages/Add.vue
@@ -1,13 +1,34 @@
 <template>
-  <h1>Add figures</h1>
+  <div class="container">
+    <div class="add-figures-section">
+      <div class="row">
+        <div class="col-3">
+          
+          
+          <b-list-group class="text-center">
+            <b-list-group-item><router-link class="nav-link" to="/AddFigures/Circle">Circle</router-link></b-list-group-item>
+            <b-list-group-item><router-link class="nav-link" to="/AddFigures/Square">Square</router-link></b-list-group-item>
+            <b-list-group-item><router-link class="nav-link" to="/AddFigures/Rectangle">Rectangle</router-link></b-list-group-item>
+            <b-list-group-item><router-link class="nav-link" to="/AddFigures/Triangle">Triangle</router-link></b-list-group-item>
+          </b-list-group>
+        </div>
+        <div class="col-9 text-center">
+          <router-view></router-view>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
+
 export default {
-  name: 'add'
+  name: 'add',
 }
 </script>
 
-<style>
-
+<style scoped>
+  .add-figures-section{
+    margin-top: 35px;   
+  }
 </style>
