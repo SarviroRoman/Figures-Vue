83d30c061bca0ee7dc2605d4afea7670e962aec7
Roman Sarviro <roman.sarviro@mail.ru>
2019-04-12 00:30:09 +0300

    Adding circles to the server was implemented

--- figures/src/components/Figures/CircleComponent.vue
+++ figures/src/components/Figures/CircleComponent.vue
@@ -6,14 +6,44 @@
 
       <div class="row form-content">
 
-        <Form class='col-md-6 mb-6'>
-          inputs
-        </Form>
+        <div class='col-md-6 mb-6'>
+          
+          <b-form @submit="addCircle" class="form-row">
+            <label for='radius' class="text-left">Radius length:</label>
+            <b-form-input 
+              id='radius' 
+              v-model.number='radius'
+              type='number'
+              placeholder='10' 
+              :min='minRadius' 
+              :max='maxRadius' 
+              step='0.1'   
+              :state="radiusState"
+            ></b-form-input>
+            <b-form-invalid-feedback id="input-live-feedback" class="text-left">
+              Plese enter valid radius
+            </b-form-invalid-feedback>
+
+            <b-button 
+              type="submit"
+              class="add-circle-button"
+              :variant='radiusState ? "success": "danger"' 
+              :disabled='!radiusState || responseIsSuccess'
+            >
+              Add Circle
+              <template v-if="responseIsSuccess">
+                <b-spinner small type="grow"></b-spinner>
+                <span class="sr-only">Loading...</span>
+              </template>
+            </b-button>
+
+          </b-form>
+        </div>
         
         <div class="col-md-6 mb-6 text-center">
           <FiguresInformation formula='S=πr²' imageName='circle.png'></FiguresInformation>
         </div>
-        
+
       </div>
       
     </div>
@@ -29,6 +59,36 @@ export default {
   components: {
     TitleMessage,
     FiguresInformation,
+  },
+  props: ['responseIsSuccess'],
+  data() {
+    return {
+      radius: 10,
+      minRadius: 0,
+      maxRadius: 10000,
+    }
+  },
+  computed: {
+    radiusState() {
+      if (this.radius > this.minRadius && this.radius <= this.maxRadius) {
+        return true;
+      }
+      return false
+    }
+  },
+  methods: {
+
+    getCircleArea() {
+      const area = Math.pow(Number(this.radius),2) * Math.PI;
+      return Math.round(area * 10000)/10000;
+    },
+
+    addCircle(evt) {
+      evt.preventDefault();
+      const area = this.getCircleArea();
+      this.$emit('addFigures', 'Circle', area);
+    },
+    
   }
 }
 </script>
@@ -36,5 +96,8 @@ export default {
 <style scope>
   .form-content{
     margin: 25px;
+  }
+  .add-circle-button{
+    margin-top: 15px;
   }
 </style>
