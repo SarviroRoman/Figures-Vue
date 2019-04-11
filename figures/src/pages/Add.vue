79e8bd9d39e75903d8fbc841b0c2416deda83467
Roman Sarviro <roman.sarviro@mail.ru>
2019-04-12 00:28:35 +0300

    Method of add figures to the server has been added

--- figures/src/pages/Add.vue
+++ figures/src/pages/Add.vue
@@ -11,17 +11,70 @@
           </b-list-group>
         </div>
         <div class="col-9 text-center">
-          <router-view></router-view>
+          <!-- <keep-alive> -->
+            <router-view v-on:addFigures='addFigures' :responseIsSuccess='responseIsSuccess'></router-view>
+          <!-- </keep-alive> -->
+
+          <b-alert
+            :show="dismissCountDown"
+            dismissible
+            variant="success"
+            @dismissed="dismissCountDown=0"
+            @dismiss-count-down="countDownChanged"
+            class="deleteAlert"
+          >
+            <p class="text-center">{{textAlertMessage}}</p>
+            <b-progress
+              variant="success"
+              :max="dismissSecs"
+              :value="dismissCountDown"
+              height="4px"
+            ></b-progress>
+          </b-alert>
+          
         </div>
       </div>
+      
     </div>
   </div>
 </template>
 
 <script>
+import axios from 'axios'
 
 export default {
   name: 'add',
+  data() {
+    return {
+      responseIsSuccess: false,
+      textAlertMessage: String,
+      showAlert: false,
+      dismissSecs: 10,
+      dismissCountDown: 0,
+    }
+  },
+  methods: {
+    addFigures(type, area){
+      this.responseIsSuccess = true;
+      const newFigure = {
+        type: type,
+        area: area
+      };
+
+      axios
+      .post(`http://localhost:7000/figures`, newFigure)
+      .then(response => {
+        if(response.data.success){
+          this.responseIsSuccess = false;
+          this.textAlertMessage = `${type} #${response.data.id} with ${area} area successfully added`;
+          this.dismissCountDown = this.dismissSecs;        
+          }
+      })
+    },
+        countDownChanged(dismissCountDown) {
+      this.dismissCountDown = dismissCountDown
+    },
+  }
 }
 </script>
 
