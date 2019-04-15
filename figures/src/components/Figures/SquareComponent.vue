12b4e2394cc9bbc24d7aaed1af70edc2943b9bd8
Roman Sarviro <roman.sarviro@mail.ru>
2019-04-15 03:24:48 +0300

    Adding squares to the server was implemented

--- figures/src/components/Figures/SquareComponent.vue
+++ figures/src/components/Figures/SquareComponent.vue
@@ -6,9 +6,38 @@
 
       <div class="row form-content">
 
-        <Form class='col-md-6 mb-6'>
-          inputs
-        </Form>
+        <div class='col-md-6 mb-6'>
+          <b-form @submit="addSquare" class="form-row">
+          <label for='square' class="text-left">Square length:</label>
+          <b-form-input 
+            id='square' 
+            v-model.number='square'
+            type='number'
+            placeholder='10' 
+            :min='minSquare' 
+            :max='maxSquare' 
+            step='0.1'   
+            :state="squareState"
+          ></b-form-input>
+          <b-form-invalid-feedback id="input-live-feedback" class="text-left">
+            Plese enter valid square
+          </b-form-invalid-feedback>
+
+          <b-button 
+            type="submit"
+            class="add-square-button"
+            :variant='squareState ? "success": "danger"' 
+            :disabled='!squareState || responseIsSuccess'
+          >
+            Add Square
+            <template v-if="responseIsSuccess">
+              <b-spinner small type="grow"></b-spinner>
+              <span class="sr-only">Loading...</span>
+            </template>
+          </b-button>
+
+        </b-form>
+      </div>
         
         <div class="col-md-6 mb-6 text-center">
           <FiguresInformation formula='S=a²' imageName='square.jpg'></FiguresInformation>
@@ -29,10 +58,45 @@ export default {
   components: {
     TitleMessage,
     FiguresInformation,
+  },
+  props: ['responseIsSuccess'],
+  data() {
+    return {
+      square: 10,
+      minSquare: 0,
+      maxSquare: 10000,
+    }
+  },
+  computed: {
+    squareState() {
+      if (this.square > this.minSquare && this.square <= this.maxSquare) {
+        return true;
+      }
+      return false
+    }
+  },
+   methods: {
+
+    getSquareArea() {
+      const area = Math.pow(Number(this.square),2);
+      return Math.round(area * 10000)/10000;
+    },
+
+    addSquare(evt) {
+      evt.preventDefault();
+      const area = this.getSquareArea();
+      this.$emit('addFigures', 'Square', area);
+    },
+    
   }
 }
 </script>
 
-<style>
-
+<style scope>
+  .form-content{
+      margin: 25px;
+  }
+  .add-square-button{
+    margin-top: 15px;
+  }
 </style>
